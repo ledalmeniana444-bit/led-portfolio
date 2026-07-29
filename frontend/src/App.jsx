8f91afd395eca14fcb36604a7447df5ea3efd049
Led Almeniana <ledalmeniana444@gmail.com>
2026-07-29 13:16:53 +0800

    Created about and projects section

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,15 @@
 import Navbar from "./components/layout/Navbar";
 import Hero from "./components/sections/Hero";
+import About from "./components/sections/About";
+import Projects from "./components/sections/Projects";
 
 function App(){
   return(
     <>
       <Navbar />
       <Hero />
+      <About/>
+      <Projects/>
     </>
   );
 }
---
+++ frontend/src/components/sections/About.jsx
@@ -0,0 +1,16 @@
+function About(){
+    return(
+        <section className="px-6 py-20">
+            <div className="mx-auto max-w-3xl">
+                <h2 className="text-3xl font-bold">
+                About me
+                </h2>
+
+                <p className="mt-5 text-gray-600">
+                    I'm a Bachelor of Science in Information Technology graduate passionate about building web applications.
+                </p>
+            </div>
+        </section>
+    );
+}
+export default About;
